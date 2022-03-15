3218f4c607d8d14ee6a1315de73e179a3df5448d
HaixiangYan <haixiang6123@gmail.com>
2022-03-15 18:48:25 +0800

    feat: 实现列的宽度改变

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,119 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Resizable } from 'react-resizable';
+import {Table} from "antd";
+import {ColumnsType} from "antd/lib/table/interface";
+
+import './styles.css';
+
+const ResizableTitle = (props: any) => {
+  const { onResize, width, ...restProps } = props;
+
+  if (!width) {
+    return <th {...restProps} />;
+  }
+
+  return (
+    <Resizable
+      width={width}
+      height={0}
+      handle={
+        <span
+          className="react-resizable-handle"
+          onClick={e => {
+            e.stopPropagation();
+          }}
+        />
+      }
+      onResize={onResize}
+      draggableOpts={{ enableUserSelectHack: false }}
+    >
+      <th {...restProps} />
+    </Resizable>
+  );
+};
 
 function App() {
+  const [columns, setColumns] = useState<ColumnsType>([
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      width: 200,
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      width: 100,
+      sorter: (a: any, b: any) => a.amount - b.amount,
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      width: 100,
+    },
+    {
+      title: 'Note',
+      dataIndex: 'note',
+      width: 100,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => <a>Delete</a>,
+    },
+  ]);
+
+  const components = {
+    header: {
+      cell: ResizableTitle,
+    },
+  };
+
+  const data = [
+    {
+      key: 0,
+      date: '2018-02-11',
+      amount: 120,
+      type: 'income',
+      note: 'transfer',
+    },
+    {
+      key: 1,
+      date: '2018-03-11',
+      amount: 243,
+      type: 'income',
+      note: 'transfer',
+    },
+    {
+      key: 2,
+      date: '2018-04-11',
+      amount: 98,
+      type: 'income',
+      note: 'transfer',
+    },
+  ];
+
+  const handleResize = (index : number) => (e: Event, { size }: { size: any }) => {
+    const nextColumns = [...columns];
+    nextColumns[index] = {
+      ...nextColumns[index],
+      width: size.width,
+    };
+    setColumns(nextColumns);
+  };
+
+  const displayColumns = columns.map((col, index) => ({
+    ...col,
+    onHeaderCell: (column: any) => ({
+      width: column.width,
+      onResize: handleResize(index),
+    }),
+  }));
+
   return (
     <div>
       <h1>react-resizable + Table</h1>
+
+      <Table bordered components={components} columns={displayColumns} dataSource={data} />;
     </div>
   );
 }
